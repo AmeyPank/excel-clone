25a565611fcadb1f11614c52d25968e2060f9c40
AmeyPank <pankantiameya@.com>
2023-04-25 14:27:00 +0530

    Excel Clone

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// document.getElementById("my-input").addEventListener("input", () => {
+//   console.log(document.getElementById("my-input").value);
+// });
+
 const thead = document.getElementById("table-heading-row");
 const tbody = document.getElementById("table-body");
 const columns = 26;
@@ -21,6 +25,18 @@ const fontFamily = document.getElementById("font-family");
 const cutBtn = document.getElementById("cut-btn");
 const copyBtn = document.getElementById("copy-btn");
 const pasteBtn = document.getElementById("paste-btn");
+
+const matrix = new Array(rows);
+// 100X26
+
+for (let i = 0; i < rows; i++) {
+  matrix[i] = new Array(columns);
+  for (let j = 0; j < columns; j++) {
+    matrix[i][j] = {};
+  }
+}
+
+console.log("Matrix", matrix);
 
 // alphabets.forEach((alphabet) => {
 //   var th = document.createElement("th");
@@ -46,16 +62,24 @@ for (let row = 0; row < rows; row++) {
     td.setAttribute("spellcheck", "false");
     td.setAttribute("id", `${String.fromCharCode(65 + col)}${row + 1}`);
     td.addEventListener("focus", (event) => onFocusFnc(event));
+    td.addEventListener("input", (event) => onInputFnc(event));
     tr.appendChild(td);
   }
   //append the row into the bodyw
   tbody.appendChild(tr);
 }
 
+function onInputFnc(event) {
+  updateJson(event.target);
+}
+
 function onFocusFnc(event) {
   console.log("In focus:", event.target);
   currCell = event.target;
   document.getElementById("current-cell").innerText = event.target.id;
+  console.log(currCell.style.cssText);
+  console.log(currCell.innerText);
+  console.log(currCell.id);
 }
 
 // event listeners for buttons
@@ -67,6 +91,7 @@ boldBtn.addEventListener("click", () => {
     currCell.style.fontWeight = "bold";
   }
   console.log("bold", currCell);
+  updateJson(currCell);
 });
 
 italicsBtn.addEventListener("click", () => {
@@ -76,6 +101,7 @@ italicsBtn.addEventListener("click", () => {
     currCell.style.fontStyle = "italic";
   }
   console.log("italics", currCell);
+  updateJson(currCell);
 });
 
 underlineBtn.addEventListener("click", () => {
@@ -85,33 +111,41 @@ underlineBtn.addEventListener("click", () => {
     currCell.style.textDecoration = "underline";
   }
   console.log("underline", currCell);
+  updateJson(currCell);
 });
 
-bgColor.addEventListener("change", () => {
+bgColor.addEventListener("input", () => {
   currCell.style.backgroundColor = bgColor.value;
+  updateJson(currCell);
 });
 
-textColor.addEventListener("change", () => {
+textColor.addEventListener("input", () => {
   currCell.style.color = textColor.value;
+  updateJson(currCell);
 });
 
 leftAlign.addEventListener("click", () => {
   currCell.style.textAlign = "left";
+  updateJson(currCell);
 });
 
 rightAlign.addEventListener("click", () => {
   currCell.style.textAlign = "right";
+  updateJson(currCell);
 });
 centerAlign.addEventListener("click", () => {
   currCell.style.textAlign = "center";
+  updateJson(currCell);
 });
 
 fontSize.addEventListener("change", () => {
   currCell.style.fontSize = fontSize.value;
+  updateJson(currCell);
 });
 
 fontFamily.addEventListener("change", () => {
   currCell.style.fontFamily = fontFamily.value;
+  updateJson(currCell);
 });
 
 cutBtn.addEventListener("click", () => {
@@ -119,6 +153,7 @@ cutBtn.addEventListener("click", () => {
     style: currCell.style.cssText,
     text: currCell.innerText,
   };
+  updateJson(currCell);
   currCell.style = null;
   currCell.innerText = null;
 });
@@ -128,9 +163,96 @@ copyBtn.addEventListener("click", () => {
     style: currCell.style.cssText,
     text: currCell.innerText,
   };
+  updateJson(currCell);
 });
 
 pasteBtn.addEventListener("click", () => {
   currCell.style.cssText = cutValue.style;
   currCell.innerText = cutValue.text;
+  updateJson(currCell);
 });
+
+// get current cells data in this format
+//  {
+// style:"",
+// text:"",
+// id:""
+// }
+
+// [
+//   [{bg},{},{}],
+//   [{},{njh},{}],
+//   [{},{njhu},{}]
+// ]
+
+// save all elements data in a json.
+
+function updateJson(cell) {
+  var json = {
+    style: cell.style.cssText,
+    text: cell.innerText,
+    id: cell.id,
+  };
+  // update this json in my matrix
+  var id = cell.id.split(""); //A1,A2,A3,A4
+
+  var i = id[1] - 1;
+  var j = id[0].charCodeAt(0) - 65;
+
+  matrix[i][j] = json;
+}
+
+function downloadJson() {
+  // Define your JSON data
+
+  // Convert JSON data to a string
+  const jsonString = JSON.stringify(matrix);
+
+  // Create a Blob with the JSON data and set its MIME type to application/json
+  const blob = new Blob([jsonString], { type: "application/json" });
+
+  // Create an anchor element and set its href attribute to the Blob URL
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "data.json"; // Set the desired file name
+
+  // Append the link to the document, click it to start the download, and remove it afterward
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+document.getElementById("jsonFile").addEventListener("change", readJsonFile);
+
+function readJsonFile(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const fileContent = e.target.result;
+
+      // {id,style,text}
+      // Parse the JSON file content and process the data
+      try {
+        const jsonData = JSON.parse(fileContent);
+        console.log("matrix2", jsonData);
+        jsonData.forEach((row) => {
+          row.forEach((cell) => {
+            if (cell.id) {
+              var myCell = document.getElementById(cell.id);
+              myCell.innerText = cell.text;
+              myCell.style.cssText = cell.style;
+            }
+          });
+        });
+        // Process the JSON data as needed
+      } catch (error) {
+        console.error("Error parsing JSON file:", error);
+      }
+    };
+
+    reader.readAsText(file);
+  }
+}
